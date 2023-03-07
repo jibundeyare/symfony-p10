c71718b081ee0a4bbbe16f168942dbaaa25a6528
jibundeyare <contact@jibundeyare.com>
2023-03-07 15:08:04 +0100

    Création de classes de fixtures et d'un script de productivité
    Ces classes n'injectent pas encorr de données mais elles contiennent
    toutes les dépendances nécessaires.
    
    Le script permet de :
    
    - supprimer la BDD
    - créer la BDD
    - créer le schéma de la BDD
    - injecter les fixtures

---
+++ src/DataFixtures/TestFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory as FakerFactory;
+use Faker\Generator as FakerGenerator;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class TestFixtures extends Fixture
+{
+    private $doctrine;
+    private $faker;
+    private $hasher;
+
+    public function __construct(ManagerRegistry $doctrine, UserPasswordHasherInterface $hasher)
+    {
+        $this->doctrine = $doctrine;
+        $this->faker = FakerFactory::create('fr_FR');
+        $this->hasher = $hasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $manager->flush();
+    }
+}
